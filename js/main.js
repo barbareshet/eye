421c3853c28769bbf2e16a860700739aa8fe8fb1
ido barnea <barbareshet@gmail.com>
2017-08-03 09:17:10 +0300

    eye movement

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
 (function($){
-    var pageY = pageYOffset;
-    var pageX = pageXOffset;
+    // var pageY = pageYOffset;
+    // var pageX = pageXOffset;
    
     var $window = $(window);
     var $main = $('#main');
+    var eye = $('#eye-in');
     $($main).mousemove(function(event){
-        console.log(pageX + " and " + pageY)    ;
+        //console.log(event.pageX + " and " + event.pageY)    ;
+        var pageY = pageYOffset/10;
+        var pageX = pageXOffset/10;
+        // console.log(eyeposx);
+        $(eye).css({
+            'top':event.pageY/2,
+            'left': event.pageX/20
+        });
     });
     
 })(jQuery);
